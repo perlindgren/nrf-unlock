279a48cc985bcc59bc8bf87a739a601ff9779ecc
Per Lindgren <per.lindgren@ltu.se>
2024-02-12 08:44:07 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use probe_rs::{Lister, Permissions};
+
+fn main() {
+    // Get a list of all available debug probes.
+    let lister = Lister::new();
+
+    let probes = lister.list_all();
+
+    // Use the first probe found.
+    let mut probe = probes[0].open(&lister).unwrap();
+
+    // Attach to a chip.
+    let mut session = probe
+        .attach("nRF52840_xxAA", Permissions::new().allow_erase_all())
+        .unwrap();
+
+    // Select a core.
+    let mut core = session.core(0).unwrap();
+
+    // Halt the attached core.
+    core.halt(std::time::Duration::from_millis(10)).unwrap();
+}
